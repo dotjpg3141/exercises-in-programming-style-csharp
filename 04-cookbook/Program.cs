0ced833a7eee264c7d84a6f6b5f733e7afbe471b
JP314 <jan.grau95@gmail.com>
2018-05-06 15:10:19 +0200

    04-cookbook

---
+++ 04-cookbook/Program.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace _04_cookbook
+{
+	static class Program
+	{
+		private static readonly List<string> words = new List<string>();
+		private static readonly HashSet<string> stopWords = new HashSet<string>();
+		private static readonly Dictionary<string, int> wordFrequencies = new Dictionary<string, int>();
+
+		static void Main(string[] args)
+		{
+			/*
+			 Constraints:
+				Larger problem decomposed in procedural abstractions
+				Larger problem solved as a sequence of commands, each corresponding to a procedure
+			 */
+
+			ReadWords(args[0]);
+			NormalizeWords();
+
+			ReadStopWords(args[2]);
+			RemoveStopWordsAndSmallWords();
+
+			CountWords();
+			PrintResult(args[1]);
+		}
+
+		static void ReadWords(string path)
+		{
+			var text = File.ReadAllText(path);
+			words.AddRange(new Regex(@"[^0-9A-Za-z]+").Split(text));
+		}
+
+		static void ReadStopWords(string path)
+		{
+			var text = File.ReadAllText(path);
+			stopWords.UnionWith(text.Split(','));
+		}
+
+		static void NormalizeWords()
+		{
+			for (int i = 0; i < words.Count; i++)
+			{
+				words[i] = words[i].ToLowerInvariant();
+			}
+		}
+
+		static void RemoveStopWordsAndSmallWords()
+		{
+			int index = words.Count - 1;
+			while (index >= 0)
+			{
+				var word = words[index];
+				if (word.Length < 2 || stopWords.Contains(word))
+				{
+					words.RemoveAt(index);
+				}
+				index--;
+			}
+		}
+
+		static void CountWords()
+		{
+			foreach (var word in words)
+			{
+				wordFrequencies.TryGetValue(word, out int count);
+				wordFrequencies[word] = count + 1;
+			}
+		}
+
+		static void PrintResult(string path)
+		{
+			var topWords = wordFrequencies.OrderByDescending(kvp => kvp.Value).Take(25);
+			using (var output = new StreamWriter(path))
+			{
+				foreach (var kvp in topWords)
+				{
+					output.WriteLine(kvp.Key + "  -  " + kvp.Value);
+				}
+			}
+		}
+	}
+}
